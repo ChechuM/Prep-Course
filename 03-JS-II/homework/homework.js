548fadb608be3247ccbd41c5b12199c73eee9444
ChechuM <ceciliammoroni@gmail.com>
2022-09-19 09:38:34 +1000

    Fixed tabla del seis

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -140,9 +140,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
-  var tabla =[6,12,18,24,30,36,42,48,54,60];
-  for (var i=0; i < tabla.length; i++) {
-      console.log (tabla[i])};
+  let tabla =[];
+  for (let i=0; i < 11; i++) {
+      tabla.push(6*i);
+}
+return tabla
 }
 
 function tieneTresDigitos(numero){
